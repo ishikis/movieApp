466d156fd7a65e41f43b3d1c3c9cd572846dfcdc
ishikis <ishik.is@hotmail.com>
2019-08-25 00:26:41 +0300

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   //tüm componentleri desc alanına ekliyoruz
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
